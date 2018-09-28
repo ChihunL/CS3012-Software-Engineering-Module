6eef6d96d5af94c9fa6f10f84b91e5ba690b53df
Chihun Lee <Chihun@live.ie>
2018-09-28 22:52:17 +0100

    Changed configurations

--- Development Task 1(LCA)/lca_unittest.py
+++ Development Task 1(LCA)/lca_unittest.py
@@ -1,10 +1,9 @@
 import unittest
 import Node
-from lca import findLCA
-from lca import findPath
-from lca import Node
+from lca import *
 
 class TestLCA(unittest.TestCase):
 
     def setUp(self):
         pass
+    
