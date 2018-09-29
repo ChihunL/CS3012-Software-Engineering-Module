bcef56b7a0dede6d8e94ce83be074b07460aca05
Chihun Lee <Chihun@live.ie>
2018-09-29 23:51:04 +0100

    Made tests for to find lca for when there is positive matches

--- Development Task 1(LCA)/lca_unittest.py
+++ Development Task 1(LCA)/lca_unittest.py
@@ -1,9 +1,23 @@
 import unittest
-import Node
 from lca import *
 
 class TestLCA(unittest.TestCase):
 
     def setUp(self):
         pass
-    
+
+    def test_find_lca(self):
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.right.left = Node(6)
+        root.right.right = Node(7)
+        self.assertEqual(2, findLCA(root, 4, 5))
+        self.assertEqual(1, findLCA(root, 4, 6))
+        self.assertEqual(1, findLCA(root, 3, 4))
+        self.assertEqual(2, findLCA(root, 2, 4))
+
+if __name__ == '__main__':
+    unittest.main()
