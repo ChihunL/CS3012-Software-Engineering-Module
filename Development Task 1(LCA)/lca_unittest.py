65b71742a425315eb942ec3f98ded11b45c226b4
Chihun Lee <Chihun@live.ie>
2018-10-01 23:22:34 +0100

    Added tests for lca with 3 nodes, made some method format naming changes

--- Development Task 1(LCA)/lca_unittest.py
+++ Development Task 1(LCA)/lca_unittest.py
@@ -7,12 +7,15 @@ class TestLCA(unittest.TestCase):
         pass
 
     def test_null_graph(self):
-        self.assertEqual(-1, findLCA(root, 4, 5)) 
-        self.assertEqual(-1, findLCA(root, 2, 7))   
+        tree = Tree()
+        self.assertEqual(-1, Tree.findLCA(root, 4, 5)) 
+        self.assertEqual(-1, Tree.findLCA(root, 2, 7))
+           
 
     def test_finding_lca_itself(self):
         root = Node(1)
-        self.assertEqual(1, findLCA(root, 1, 1))
+        tree = Tree()
+        self.assertEqual(1, Tree.findLCA(root, 1, 1))
 
     def test_find_lca(self):
         root = Node(1)
@@ -22,12 +25,13 @@ class TestLCA(unittest.TestCase):
         root.left.right = Node(5)
         root.right.left = Node(6)
         root.right.right = Node(7)
-        self.assertEqual(2, findLCA(root, 4, 5))
-        self.assertEqual(1, findLCA(root, 4, 6))
-        self.assertEqual(1, findLCA(root, 3, 4))
-        self.assertEqual(2, findLCA(root, 2, 4))
-        self.assertNotEqual(7, findLCA(root, 3, 4))
-        self.assertNotEqual(5, findLCA(root, 2, 4))
+        tree = Tree()
+        self.assertEqual(2, Tree.findLCA(root, 4, 5))
+        self.assertEqual(1, Tree.findLCA(root, 4, 6))
+        self.assertEqual(1, Tree.findLCA(root, 3, 4))
+        self.assertEqual(2, Tree.findLCA(root, 2, 4))
+        self.assertNotEqual(7, Tree.findLCA(root, 3, 4))
+        self.assertNotEqual(5, Tree.findLCA(root, 2, 4))
 
     
 
