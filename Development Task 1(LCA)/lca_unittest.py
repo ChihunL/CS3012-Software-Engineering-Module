9677b4cef33faf41714f8a33199ee5f1ca3472ce
Chihun Lee <Chihun@live.ie>
2018-10-11 22:36:32 +0100

    Made changes to how tests are run through LCA

--- Development Task 1(LCA)/lca_unittest.py
+++ Development Task 1(LCA)/lca_unittest.py
@@ -13,21 +13,20 @@ class TestLCA(unittest.TestCase):
 
     def test_finding_lca_itself(self):
         root = Node(1)
-        tree = Tree()
-        self.assertEqual(1, Tree.findLCA(root, 1, 1))
+        self.assertEqual(1, findLCA(root, 1, 1))
     
     def test_lca_three_nodes(self):
         root = Node(1)
         root.left = Node(2)
         root.right = Node(3)
-        self.assertEqual(1, Tree.findLCA(root, 2, 3))
+        self.assertEqual(1, findLCA(root, 2, 3))
 
     def test_lca_child_ancestor(self):
         root = Node(1)
         root.left = Node(2)
         root.left.left = Node(3)
-        self.assertEqual(2, Tree.findLCA(root, 2, 3))
-        self.assertEqual(1, Tree.findLCA(root, 1, 3))
+        self.assertEqual(2, findLCA(root, 2, 3))
+        self.assertEqual(1, findLCA(root, 1, 3))
 
 
     def test_find_lca(self):
@@ -39,20 +38,19 @@ class TestLCA(unittest.TestCase):
         root.right.left = Node(6)
         root.right.right = Node(7)
         tree = Tree()
-        self.assertEqual(2, Tree.findLCA(root, 4, 5))
-        self.assertEqual(1, Tree.findLCA(root, 4, 6))
-        self.assertEqual(1, Tree.findLCA(root, 3, 4))
-        self.assertEqual(2, Tree.findLCA(root, 2, 4))
-        self.assertNotEqual(7, Tree.findLCA(root, 3, 4))
-        self.assertNotEqual(5, Tree.findLCA(root, 2, 4))
+        self.assertEqual(2, findLCA(root, 4, 5))
+        self.assertEqual(1, LCA.findLCA(root, 4, 6))
+        self.assertEqual(1, LCA.findLCA(root, 3, 4))
+        self.assertEqual(2, LCA.findLCA(root, 2, 4))
+        self.assertNotEqual(7, LCA.findLCA(root, 3, 4))
+        self.assertNotEqual(5, LCA.findLCA(root, 2, 4))
 
     def test_dag_lca(self):
-        tree = Tree()
     
     def test_not_connected(self):
-        root = LCA.Node(1)
-        root.left = LCA.Node(2)
-        root.right = LCA.Node(3)
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
         root.left.left = LCA.Node(4)
         self.assertEqual(-1, LCA.findLCA(root, 2, 9, ))
         
