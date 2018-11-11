8aec8a57172bf6efacdee2308037d9750a4c5790
Chihun Lee <Chihun@live.ie>
2018-11-11 09:38:13 +0000

    Added ability create database and Input username and password

--- Github Access/gitaccess.py
+++ Github Access/gitaccess.py
@@ -1,11 +1,28 @@
 from github import Github
-
-# First create a Github instance:
-
-# using username and password
-g = Github("ChihunL", "********")
+import sqlite3
+from sqlite3 import Error
 
 
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+
+
+
+if __name__ == '__main__':
+
+    username = raw_input('Enter in Username: ')
+    password = raw_input('Enter in Password: ')
+
+    g = Github(username, password)
+
+    create_connection("RepositoriesNames.db")
+    # Then play with your Github objects:
+    for repo in g.get_user().get_repos():
+        print(repo.name)
