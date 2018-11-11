4dcd768d3243977fd84bd14add9324b6e8a753d5
Chihun Lee <Chihun@live.ie>
2018-11-11 10:00:22 +0000

    The program now stores the list of repo names into a csv file

--- Github Access/gitaccess.py
+++ Github Access/gitaccess.py
@@ -1,6 +1,7 @@
 from github import Github
 import sqlite3
 from sqlite3 import Error
+import csv
 
 
 def create_connection(db_file):
@@ -13,7 +14,21 @@ def create_connection(db_file):
     finally:
         conn.close()
 
+def create_table(conn, project_names):
+ 
+    sql = ''' INSERT INTO projectNames(projectName)
+              VALUES(?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project_names)
+    return cur.lastrowid
 
+def create_repo_list(conn, repo_list):
+ 
+    sql = ''' INSERT INTO projectNames(projectName)
+              VALUES(?) '''
+    cur = conn.cursor()
+    cur.execute(sql, repo_list)
+    return cur.lastrowid
 
 if __name__ == '__main__':
 
@@ -22,7 +37,10 @@ if __name__ == '__main__':
 
     g = Github(username, password)
 
-    create_connection("RepositoriesNames.db")
-    # Then play with your Github objects:
-    for repo in g.get_user().get_repos():
-        print(repo.name)
+    with open('repoNames.csv', 'wb') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    
+        for repo in g.get_user().get_repos():
+            filewriter.writerow([repo.name])
