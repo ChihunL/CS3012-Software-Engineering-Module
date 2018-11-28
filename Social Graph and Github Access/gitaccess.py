9257977ab1054c71233132d87562062b0daa2a0c
Chihun Lee <Chihun@live.ie>
2018-11-28 20:04:45 +0000

    Added extraction of different data

--- Social Graph and Github Access/gitaccess.py
+++ Social Graph and Github Access/gitaccess.py
@@ -32,15 +32,44 @@ def create_repo_list(conn, repo_list):
 
 if __name__ == '__main__':
 
-    username = raw_input('Enter in Username: ')
-    password = raw_input('Enter in Password: ')
+    #username = raw_input('Enter in Username: ')
+    #password = raw_input('Enter in Password: ')
 
-    g = Github(username, password)
+    g = Github("3944f1ef76fcd722b15e6d35a11e2fa93e01485f")
 
-    with open('repoNames.csv', 'wb') as csvfile:
+    for repo in g.get_user().get_repos():
+        print repo.name
+
+    user = g.get_user()
+    print user.login
+    repo = g.get_repo("ChihunL/First-Year-Java-Programming-Assignments")
+    print repo.name
+
+    repo = g.get_repo("ChihunL/First-Year-Java-Programming-Assignments")
+    print (repo.get_topics())
+
+    repo = g.get_repo("ChihunL/First-Year-Java-Programming-Assignments")
+    print repo.stargazers_count
+
+    labels = repo.get_labels()
+    for label in labels:
+        print(label)
+
+"""    repositories = g.search_repositories(query='language:java')
+    for repo in repositories:
+        print(repo)
+
+    repositories = g.search_repositories(query='good-first-issues:>3')
+    for repo in repositories:
+        print(repo)"""
+    
+    
+
+
+"""    with open('repoNames.csv', 'wb') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-    
         for repo in g.get_user().get_repos():
             filewriter.writerow([repo.name])
+"""
