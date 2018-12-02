b6cf1b606c8bb88687fc1aa815ee2e8be6d614bd
Chihun Lee <Chihun@live.ie>
2018-12-02 23:22:32 +0000

    Fixed program not compiling

--- Social Graph and Github Access/visuals/src/app/app.component.ts
+++ Social Graph and Github Access/visuals/src/app/app.component.ts
@@ -14,25 +14,9 @@ export class AppComponent {
     token: '3944f1ef76fcd722b15e6d35a11e2fa93e01485f'
   
  });
-str = this.readTextFile('/home/chihun/Documents/SoftwareEngineering/CS3012-Software-Engineering-Module/Social Graph and Github Access/repoDetails.csv'); 
+
 me = this.gh.getUser(); // no user specified defaults to the user for whom credentials were provide
 
-readFile(file: File) {
-  var reader = new FileReader();
-  reader.onload = () => {
-      console.log(reader.result);
-  };
-  reader.readAsText(file);
-}
-readTextFile(filepath) {
-	var str = "";
-	var txtFile = new File([""],filepath);
-	txtFile.open("r");
-	while (!txtFile.eof) {
-		// read each line of text
-		str += txtFile.readln() + "\n";
-	}
-	return str;
-}
+
 }
 
